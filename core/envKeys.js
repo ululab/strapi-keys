eae9e5adec13377b2e255de63f7650b71cc75847
Andrea Ceccato <andrea.ceccato@ululab.it>
2023-12-28 20:29:32 +0100

    envKeys

--- core/envKeys.js
+++ core/envKeys.js
@@ -3,12 +3,10 @@
  * 
  * @type {Array<{name: string, type: string, value: string|null, processed: boolean}>}
  */
-const envKeysToGenerate = [
+module.exports = [
     { name: 'APP_KEYS', type: 'array:4<string:19>', value: null, processed: false },
     { name: 'API_TOKEN_SALT', type: 'string:31', value: null, processed: false },
     { name: 'ADMIN_JWT_SECRET', type: 'string:31', value: null, processed: false },
     { name: 'TRANSFER_TOKEN_SALT', type: 'string:31', value: null, processed: false },
     { name: 'JWT_SECRET', type: 'string:31', value: null, processed: false },
-  ];
-
-module.exports = envKeysToGenerate;     
+]; 
