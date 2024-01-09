afde83191cb4111f3ef4ab3a9b7512a2303d4f72
Andrea Ceccato <andrea.ceccato@ululab.it>
2024-01-09 16:03:29 +0100

    Rename variable command options list

--- core/command/options.js
+++ core/command/options.js
@@ -1,21 +1,21 @@
-const commadList = require("./list");
+const commandList = require("./list");
 
 const processArgv = process.argv.slice(2);
 
 const options = {};
 
 // Options
-for (const command of commadList.filter(e => !e.withValues)) {
+for (const command of commandList.filter(e => !e.withValues)) {
   options[command.value.replace(/-/g, "")] = processArgv.includes(command.value);
 }
 
 // Options with values
-for (const commad of commadList.filter(e => e.withValues)) {
-  let flagValue = processArgv.find((e) => e.startsWith(commad.value));
+for (const command of commandList.filter(e => e.withValues)) {
+  let flagValue = processArgv.find((e) => e.startsWith(command.value));
   if (flagValue) {
     flagValue = flagValue.split('=')[1];
   }
-  options[commad.value.replace(/[-=]/g, "")] = flagValue ? flagValue.split(',') : [];
+  options[command.value.replace(/[-=]/g, "")] = flagValue ? flagValue.split(',') : [];
 }
 
 // Set default operation to generate
