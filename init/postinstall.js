b37af4965adfa4096d64448e24a15f242f7242d3
AlbertoFaveroUlulab <146853960+AlbertoFaveroUlulab@users.noreply.github.com>
2024-01-18 15:41:20 +0100

    testing package.json path

--- init/postinstall.js
+++ init/postinstall.js
@@ -4,41 +4,42 @@ const path = require('path');
 
 console.log('POST INSTALL');
 
-fs.writeFileSync('postinstall.log', 'Test');
-return;
-/*
+// return;
+
 
 // Percorso del file package.json
 const packageJsonPath = path.join(process.cwd(), 'package.json');
 
-// Nuovo script da aggiungere
-const newScript = {
-  scripts: {
-    "strapi-keys": "strapi-keys"
-  }
-};
+console.log(packageJsonPath)
+fs.writeFileSync('postinstall.log', packageJsonPath);
+return;
+// // Nuovo script da aggiungere
+// const newScript = {
+//   scripts: {
+//     "strapi-keys": "strapi-keys"
+//   }
+// };
 
-// Leggi il file package.json
-fs.readFile(packageJsonPath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(`Errore nella lettura del file package.json: ${err}`);
-    process.exit(1);
-  }
+// // Leggi il file package.json
+// fs.readFile(packageJsonPath, 'utf-8', (err, data) => {
+//   if (err) {
+//     console.error(`Errore nella lettura del file package.json: ${err}`);
+//     process.exit(1);
+//   }
 
-  // Analizza il contenuto JSON
-  const packageJson = JSON.parse(data);
+//   // Analizza il contenuto JSON
+//   const packageJson = JSON.parse(data);
 
-  // Aggiungi il nuovo script
-  Object.assign(packageJson, newScript);
+//   // Aggiungi il nuovo script
+//   Object.assign(packageJson, newScript);
 
-  // Scrivi il file package.json aggiornato
-  fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8', (err) => {
-    if (err) {
-      console.error(`Errore nella scrittura del file package.json: ${err}`);
-      process.exit(1);
-    }
+//   // Scrivi il file package.json aggiornato
+//   fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8', (err) => {
+//     if (err) {
+//       console.error(`Errore nella scrittura del file package.json: ${err}`);
+//       process.exit(1);
+//     }
 
-    console.log('Aggiunta dello script "strapi-keys" completata con successo.');
-  });
-});
-*/
+//     console.log('Aggiunta dello script "strapi-keys" completata con successo.');
+//   });
+// });
