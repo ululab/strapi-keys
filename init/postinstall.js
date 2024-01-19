8de2042b844f0af75acff322147d291873b07b9f
AlbertoFaveroUlulab <146853960+AlbertoFaveroUlulab@users.noreply.github.com>
2024-01-19 16:58:04 +0100

    Creazione degli scripts se non esistono (anche se non esiste "scripts"), formattazione e divisione in funzioni del codice

--- init/postinstall.js
+++ init/postinstall.js
@@ -1,25 +1,56 @@
 const fs = require("fs");
 const path = require("path");
 
-// Percorso del file package.json
-const packageJsonPath = path.join(process.cwd(), "../../package.json");
 
-if (!fs.existsSync(packageJsonPath)) {
-  fs.writeFileSync("postinstall.log", "package.json is not found");
-  return;
+function readPackageJson () { 
+
+  const packageJsonPath = path.join(process.cwd(), "../../package.json");
+  console.log(packageJsonPath);
+
+  if (!fs.existsSync(packageJsonPath)) {
+    fs.writeFileSync("postinstall.log", "package.json is not found");
+    return;
+  }
+
+  try {
+    return fs.readFileSync(packageJsonPath, "utf-8");
+  } catch (error) {
+    console.error(error);
+    return fs.writeFileSync(
+      "postinstall.log",
+      "package.json has not been properly read"
+    );
+  }
 }
 
-try {
-  var content = fs.readFileSync(packageJsonPath, "utf-8");
-  return fs.writeFileSync("postinstall.log", content);
-} catch (error) {
-  console.error(error);
-  return fs.writeFileSync(
-    "postinstall.log",
-    "package.json has not been properly read"
-  );
+function prepareNewScriptsContent(){
+
+  let { scripts } = JSON.parse(readPackageJson());
+
+  if (!scripts) {
+    let scriptsContent = generateNewScripts();
+    scripts = scriptsContent
+  }
+
+  const keyFound = Object.keys(scripts).find(key => key === 'strapi-keys');
+
+  if (!keyFound) { 
+    scripts['strapi-keys'] = 'strapi-keys'
+  }
+  return scripts;
 }
 
+  function generateNewContent(){
+    return `"strapi-keys": "strapi-keys"`
+  }
+
+  function generateNewScripts(){
+    return `"scripts": { \n\t` + generateNewContent() + `\n}` 
+  }
+
+  console.log(prepareNewScriptsContent());
+
+return;
 // // Nuovo script da aggiungere
 // const newScript = {
 //   scripts: {
